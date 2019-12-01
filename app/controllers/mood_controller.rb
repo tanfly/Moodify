247263edf37aa4d9997cff65d011c6e8e1cda527
tanfly <mcewen.stella@gmail.com>
2019-11-30 21:52:35 -0600

    added mood, photo and user controllers

---
+++ app/controllers/mood_controller.rb
@@ -0,0 +1,2 @@
+class MoodController < ApplicationController
+end
---
+++ test/controllers/mood_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class MoodControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
